5bf07046b2ec28e13209cecad05cdb90ae29297d
Priestly <priestlyadejo@gmail.com>
2023-06-25 02:23:49 +0100

    Made final changes to console script

--- src/scripts/js/Game.js
+++ src/scripts/js/Game.js
@@ -7,7 +7,7 @@ export default class Game {
         this.gameBoard = new GameBoard();
         this.players = [
         new Player('Player 1', 'X'),
-        new Player('Player 2', 'O')
+        new Player('CPU', 'O')
         ];
 
         // Put players in array handle turns by indexing array
@@ -38,15 +38,22 @@ export default class Game {
     }
         
     getNextMove() {
-        console.log(`${this.currentPlayer.name}, It's now your turn!`)
-        let playerRow = prompt(`Please enter a row - (1-${this.gameBoard.cells.length})`)-1
-        let playerCol = prompt(`Please enter a col - (1-${this.gameBoard.cells.length})`)-1
-        
-        if (this.isValidMove(playerRow, playerCol)) {
-            this.gameBoard.putPiece(playerRow, playerCol, this.currentPlayer.symbol)
+        if (this.currentPlayer.name != "CPU") {
+            console.log(`${this.currentPlayer.name}, It's now your turn!`)
+            let playerRow = prompt(`${this.currentPlayer.name}, please enter a row - (1-${this.gameBoard.cells.length})`)-1
+            let playerCol = prompt(`${this.currentPlayer.name}, please enter a col - (1-${this.gameBoard.cells.length})`)-1
+            
+            if (this.isValidMove(playerRow, playerCol)) {
+                this.gameBoard.putPiece(playerRow, playerCol, this.currentPlayer.symbol)
+            } else {
+                console.log("Invalid move, please try again.")
+                this.getNextMove()
+            }
         } else {
-            console.log("Invalid move, please try again.")
-            this.getNextMove()
+            let playerRow = Math.floor(Math.random()*(this.gameBoard.dims));
+            let playerCol = Math.floor(Math.random()*(this.gameBoard.dims));
+            alert("Compter is Playing Move")
+            this.gameBoard.putPiece(playerRow, playerCol, this.currentPlayer.symbol)
         }
     }
     
